0251ae5136e390c23c17f6c69f5db08692249021
Leo <leo@xuebin.me>
2022-08-01 09:19:57 +0800

    update

---
+++ slidev-uniapp/iframes/monaco/assets/html.worker.7ad66bff.js
@@ -0,0 +1,1 @@
+function e(){return new Worker("/slidev-uniapp/iframes/monaco/assets/html.worker.ae09e20d.js",{type:"module"})}export{e as default};
---
+++ slidev-uniapp/iframes/monaco/assets/json.worker.675601df.js
@@ -0,0 +1,1 @@
+function e(){return new Worker("/slidev-uniapp/iframes/monaco/assets/json.worker.cc26763b.js",{type:"module"})}export{e as default};
